67fac199d8fb58fea9688eb7c2eea57a6294f265
seokju-na <seokmaTD@gmail.com>
2017-02-07 14:59:12 +0900

    init: startup project

---
+++ README.md
@@ -0,0 +1,30 @@
+# REAL 5th HackWeek Portfolio
+
+## Download & Install
+
+```shell
+$ git clone https://github.com/startreal/hackweek-5th
+$ npm install
+```
+
+
+## Export
+
+### Export as website
+
+```shell
+$ npm run build
+$ npm run dist-website
+```
+
+
+### Export as PDF
+
+```shell
+$ npm run dist-pdf
+```
+
+
+## License
+
+GPLv3
---
+++ js/about.js
@@ -0,0 +1,78 @@
+(function () {
+    var pages = [].slice.call(document.querySelectorAll('.pages > .page'));
+    var currentPage = 0;
+    var $header = document.getElementById('header');
+
+    var revealerOpts = {
+        // the layers are the elements that move from the sides
+        nmbLayers: 3,
+        // bg color of each layer
+        bgcolor: ['#0092DD', '#fff', '#3E3A35'],
+        // effect classname
+        effect: 'anim--effect-3',
+        onStart: function (direction) {
+            // next page gets class page--animate-[direction]
+            var nextPage;
+
+            if (currentPage === pages.length - 1) {
+                nextPage = 0;
+            } else {
+                nextPage = currentPage + 1;
+            }
+
+            if (nextPage !== 0) {
+                classie.add($header, 'hide');
+            }
+
+            classie.add(pages[nextPage], 'page--animate-' + direction);
+        },
+        onEnd: function () {
+            // remove class page--animate-[direction] from next page
+            var nextPage;
+
+            if (currentPage === pages.length - 1) {
+                nextPage = 0;
+            } else {
+                nextPage = currentPage + 1;
+            }
+
+            nextPage.className = 'page';
+        }
+    };
+    var revealer = new Revealer(revealerOpts);
+
+    // clicking the page nav buttons
+    // document.querySelector('button.pagenav__button--top').addEventListener('click', function() { reveal('top'); });
+    // document.querySelector('button.pagenav__button--left').addEventListener('click', function() { reveal('left'); });
+    // document.querySelector('button.pagenav__button--right').addEventListener('click', function() { reveal('right'); });
+    document.querySelector('button.pagenav__button--bottom').addEventListener('click', function() { reveal('bottom'); });
+    // document.querySelector('button.pagenav__button--cornertopleft').addEventListener('click', function() { reveal('cornertopleft'); });
+    // document.querySelector('button.pagenav__button--cornertopright').addEventListener('click', function() { reveal('cornertopright'); });
+    // document.querySelector('button.pagenav__button--cornerbottomleft').addEventListener('click', function() { reveal('cornerbottomleft'); });
+    // document.querySelector('button.pagenav__button--cornerbottomright').addEventListener('click', function() { reveal('cornerbottomright'); });
+
+    // triggers the effect by calling instance.reveal(direction, callbackTime, callbackFn)
+    function reveal(direction) {
+        var callbackTime = 750,
+            callbackFn = function() {
+                var nextPage;
+
+                if (currentPage === pages.length - 1) {
+                    nextPage = 0;
+                } else {
+                    nextPage = currentPage + 1;
+                }
+
+                classie.remove(pages[currentPage], 'page--current');
+                classie.add(pages[nextPage], 'page--current');
+
+                currentPage = nextPage;
+
+                if (currentPage === 0) {
+                    classie.remove($header, 'hide');
+                }
+            };
+
+        revealer.reveal(direction, callbackTime, callbackFn);
+    }
+})();
